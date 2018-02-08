7efc8c9cbe7e42fa6862e99f2662b7ea0489cedb
Jim Wright <jw@jimbo.io>
2018-02-08 20:49:50 +0000

    file input to base64 example

---
+++ src/components/Upload.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="image-upload">
+    <input
+      type="file"
+      accept="image/*"
+      capture="environment"
+      @change="onImageCaptured($event.target.name, $event.target.files)">
+    <img v-bind:src="imgURL" v-if="imgURL" style="width:200px">
+  </div>
+</template>
+<script>
+export default {
+  name: 'upload',
+  data() {
+    return {
+      imgURL: null,
+    };
+  },
+  methods: {
+    getBase64Image(file) {
+      return new Promise((resolve, reject) => {
+        const fileReader = new FileReader();
+        fileReader.addEventListener('load', () => {
+          resolve(fileReader.result);
+        });
+        fileReader.addEventListener('error', (err) => {
+          reject(err);
+        });
+        fileReader.readAsDataURL(file);
+      });
+    },
+    onImageCaptured(name, files) {
+      this.getBase64Image(files[0])
+        .then((url) => {
+          this.imgURL = url;
+        });
+    },
+  },
+};
+</script>
+<style>
+</style>
